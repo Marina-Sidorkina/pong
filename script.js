3e474398fba6df9658ea1ad7e9d9e9dfca8972ad
Marina Sidorkina <m.sidorkina55@gmail.com>
2020-11-25 00:34:31 +0300

    Add ball.

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const player = new paddle(5, 200, 25, 100);
 const ai = new paddle(610, 200, 25, 100);
-
-function tick() {
-  draw();
-  window.setTimeout('tick()', 1000/60);
-}
+const ball = {x: 320, y: 240, radius: 3, xSpeed: 2, ySpeed: 0};
 
 function paddle(x, y, width, height) {
   this.x = x;
@@ -20,11 +16,30 @@ function renderPaddle(paddle) {
   ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height)
 }
 
+function renderBall(ball) {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0.2 * Math.PI, false);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+}
+
+function updateGame() {
+  ball.x += ball.xSpeed;
+  ball.y += ball.ySpeed;
+}
+
 function draw() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, 640, 480);
   renderPaddle(player);
   renderPaddle(ai);
+  renderBall(ball);
+}
+
+function tick() {
+  updateGame();
+  draw();
+  window.setTimeout('tick()', 1000/60);
 }
 
 tick();
