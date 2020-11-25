301b95d77536b94e4f34d2b822d58c840ff9b157
Marina Sidorkina <m.sidorkina55@gmail.com>
2020-11-25 15:48:35 +0300

    Add ball hitting.

--- script.js
+++ script.js
@@ -2,13 +2,36 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const player = new paddle(5, 200, 25, 100);
 const ai = new paddle(610, 200, 25, 100);
-const ball = {x: 320, y: 240, radius: 3, xSpeed: 2, ySpeed: 0};
+const ball = {
+  x: 320,
+  y: 240,
+  radius: 3,
+  xSpeed: 2,
+  ySpeed: 0,
+  reverseX: function() {
+    this.xSpeed *= -1;
+  },
+  reverseY: function() {
+    this.ySpeed *= -1;
+  }
+  };
 
 function paddle(x, y, width, height) {
   this.x = x;
   this.y = y;
   this.width = width;
   this.height = height;
+  this.hasCollideWith = function(ball) {
+    const paddleLeftWall = this.x;
+    const paddleRightWall = this.x + this.width;
+    const papaddleTopWall = this.y;
+    const paddleBottomWall = this.y + this.height;
+    if(ball.x > paddleLeftWall && ball.x < paddleRightWall
+      && ball.y > papaddleTopWall && ball.y < paddleBottomWall) {
+        return true;
+    }
+    return false;
+  }
 }
 
 function renderPaddle(paddle) {
@@ -26,6 +49,9 @@ function renderBall(ball) {
 function updateGame() {
   ball.x += ball.xSpeed;
   ball.y += ball.ySpeed;
+  if(player.hasCollideWith(ball) || ai.hasCollideWith(ball)) {
+    ball.reverseX();
+  }
 }
 
 function draw() {
